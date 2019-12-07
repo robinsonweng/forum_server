36683474c6cab7c5ed6f1013141c048ffd5c12c4
robinsonweng <robinsonweng100@gmail.com>
2019-12-07 23:56:53 +0900

    template -> templates

--- homepage.py
+++ homepage.py
@@ -1,10 +1,12 @@
 from flask import Flask,render_template
 
-app=Flask(__name__,template_folder='template',static_folder='static')
+
+app=Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
 
 if  __name__== '__main__':
         app.run(debug=True)
