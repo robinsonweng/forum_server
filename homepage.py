84e019eabae88800ce2adecb3081f80e7bebd889
robinsonweng <robinsonweng100@gmail.com>
2019-12-29 12:12:18 +0900

    dbconnect

--- homepage.py
+++ homepage.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask import render_template,url_for,request,redirect
+from module.dbconnect import DBConnect as dbconnect
 import time
+
 
 app=Flask(__name__,template_folder='template',static_url_path='/static')
 
-bottle=[]
 
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
-
-
 
 @app.route('/forum',methods=['GET','POST'])
 def forum():
@@ -22,20 +21,38 @@ def forum():
             comment = request.form.get("comment_name")
             date=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
 
-            bottle.append({"username":username,"email":email,"subject":subject,"comment":comment,"date":date})
-    
+            # check if user exsist
+            with dbconnect() as session:
+                uname = session.execute('SELECT uname FROM user')
+                if username not in uname:
+                    session.execute(f'INSERT INTO user (uid, uname, email) VALUES ({uname.rowcount+1}, "{username}", "{email}")')
+            # insert posts
+            with dbconnect() as session:
+                uid = session.execute(f'SELECT uid FROM user WHERE uname = "{username}"').fetchone()[0]
+                pid = session.execute(f'SELECT pid FROM post').rowcount
+                session.execute(f'INSERT INTO post (uid, pid, title, context, date)\
+                                  VALUES({uid}, {pid+1}, "{subject}", "{comment}", "{date}")')
             return redirect(url_for('forum'))
-
         elif 'HomepageButton' in request.form:
             return redirect(url_for('homepage'))
+
     if request.method=="GET":
+        with dbconnect() as session:
+            posts = session.execute(f'SELECT uname, email, title, context, date\
+                                      FROM post, user\
+                                      WHERE post.uid = user.uid'
+            )
+            bottle = []
+            for post in posts:
+                bottle.append({"username":post[0],
+                               "email":post[1],
+                               "subject":post[2],
+                               "comment":post[3],
+                               "date":post[4]
+                })
         return render_template('forum.html',says=bottle)
-
-
     
-     
     return render_template("forum.html")
-
 
 
 if  __name__== '__main__':
---
+++ module/dbconnect.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+class DBConnect(object):
+    """using context manager to connect database
+    """
+    def __init__(self):
+        """connect to database when initalize 
+        """
+        self.conn = sqlite3.connect('forum.db')
+
+    def __enter__(self):
+        """create the cursor when in the with statement
+        """
+        cursor = self.conn.cursor()
+        return cursor
+
+    def __exit__(self, *args):
+        """disconnect database when leave with statement
+        """
+        self.conn.commit()
+        self.conn.close()
+
+
+if __name__ == "__main__":
+    with DBConnect() as session:
+        users = session.execute('SELECT * FROM user')
+        posts = session.execute('SELECT * FROM post')
